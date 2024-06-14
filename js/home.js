9f03d07e6b64f2f3d5b87ff7d5d93caf1f4948bf
Felipe Guimarães <felipe700guima@gmail.com>
2024-06-13 22:51:36 -0300

    nav responsivo

---
+++ js/home.js
@@ -0,0 +1,7 @@
+function openMenu(){
+    menuHamburguer = document.getElementById('nav-bar').style.height='100vh'
+}
+function closeMenu(){
+    menuHamburguer = document.getElementById('nav-bar').style.height='0px'
+}
+
