0347b6e3a7a62363f6ce1bd84db5e7092bd6f948
Felipe Guimarães <felipe700guima@gmail.com>
2024-06-15 10:50:16 -0300

    iniciando animações com javascript

--- js/home.js
+++ js/home.js
@@ -5,3 +5,25 @@ function closeMenu(){
     menuHamburguer = document.getElementById('nav-bar').style.height='0px'
 }
 
+
+// ANIMATION
+
+const cards = document.querySelectorAll('.container-project-iten')
+
+window.addEventListener('scroll', checkBoxes)
+    
+checkBoxes()
+
+    function checkBoxes(){
+        const triggerBottom = window.innerHeight / 5 * 4
+
+        cards.forEach(card => {
+            const cardTop = card.getBoundingClientRect().top
+            
+            if(cardTop < triggerBottom){
+                cards.classList.add('show')
+            } else{
+                cards.classList.remove('show')
+            }
+        })
+    }
